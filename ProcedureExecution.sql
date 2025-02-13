2021b23af5e474d816cdc8f5618f4dab5ba44491
Scelo Khoza <scelo.khoza@bbd.co.za>
2025-02-13 21:14:44 +0200

    implementing logic for our stored procedures, the first procedure is for creating a project and assigning a owner

---
+++ ProcedureExecution.sql
@@ -0,0 +1,11 @@
+
+--- For CREATING A PROJECT AND ASSIGNING A OWNER
+
+EXEC sproc_CreateProject
+    @ProjectName = '',
+    @ProjectDescription = '',
+    @UserID = 123;
+---------------------------------------
+
+
+
