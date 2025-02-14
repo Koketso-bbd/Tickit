20e60a00893f8cf60d51790b844fc214dbab6224
Andy Nkumane <Andy.Nkumane@bbd.co.za>
2025-02-14 08:40:00 +0200

    added is user in project function

--- db_functions.sql
+++ db_functions.sql
@@ -10,3 +10,19 @@ GO
 
 -- SELECT dbo.fn_GetTaskCount(2);
 -- GO
+
+CREATE FUNCTION dbo.fn_IsUserInProject(@UserID INT, @ProjectID INT)
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @Exists BIT;
+    IF EXISTS (SELECT 1 FROM UserProjects WHERE UserID = @UserID AND ProjectID = @ProjectID)
+        SET @Exists = 1;
+    ELSE
+        SET @Exists = 0;
+    RETURN @Exists;
+END;
+GO
+
+-- SELECT dbo.fn_IsUserInProject(2,1);
+-- GO
