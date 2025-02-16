3d2671f1fdf8d836312c0ef9ea251db434983938
Andy Nkumane <Andy.Nkumane@bbd.co.za>
2025-02-16 14:23:18 +0200

    db functions - added get task assignee

--- database/CreateFunction.sql
+++ database/CreateFunction.sql
@@ -118,9 +118,9 @@ CREATE FUNCTION dbo.fn_GetProjectMemberCount(@ProjectID INT)
 RETURNS INT
 AS
 BEGIN
-    DECLARE @ProjectMemberCOunt INT;
-    SELECT @ProjectMemberCOunt = COUNT(*) FROM UserProjects WHERE ProjectID = @ProjectID;
-    RETURN @ProjectMemberCOunt;
+    DECLARE @ProjectMemberCount INT;
+    SELECT @ProjectMemberCount = COUNT(*) FROM UserProjects WHERE ProjectID = @ProjectID;
+    RETURN @ProjectMemberCount;
 END;
 GO
 
@@ -151,8 +151,18 @@ CREATE FUNCTION dbo.fn_GetProjectOwner(@ProjectID INT)
 RETURNS INT
 AS
 BEGIN
-    DECLARE @OwnerID ID;
-    SELECT @OwnerID = OwnerID FROM Projects WHERE ID = @ProjectID;
-    RETURN @OwnerID;
+    DECLARE @ProjectOwner INT;
+    SELECT @ProjectOwner = OwnerID FROM Projects WHERE ID = @ProjectID;
+    RETURN @ProjectOwner;
+END;
+GO
+
+CREATE FUNCTION dbo.fn_GetTaskAssignee(@TaskID INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @TaskAssignee INT;;
+    SELECT @TaskAssignee = AssigneeID FROM Tasks WHERE ID = @TaskID;
+    RETURN @TaskAssignee;
 END;
 GO
