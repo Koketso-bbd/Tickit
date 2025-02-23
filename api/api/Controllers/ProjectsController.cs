3de753bc550c2f01c881fe86ab0bad7d17a0c649
Bradley <Bradley.Ranaka@bbd.co.za>
2025-02-23 11:29:09 +0200

    Added an endpoint to test getting all projects. There are not users being returned is currently null

---
+++ api/api/Controllers/ProjectsController.cs
@@ -0,0 +1,52 @@
+using api.Data;
+using api.DTOs;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+
+        private readonly TickItDbContext _context;
+        private readonly ILogger<ProjectsController> _logger;
+
+        public ProjectsController(TickItDbContext context, ILogger<ProjectsController> logger) 
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProjectDTO>>> GetProjects()
+        {
+            try
+            {
+                var projects = await _context.Projects
+                    .Select(p => new ProjectDTO
+                    {
+                        ID = p.Id,
+                        ProjectName = p.ProjectName,
+                        ProjectDescription = p.ProjectDescription,
+                        OwnerID = p.OwnerId,
+                        Owner = new UserDTO { ID = p.Owner.Id, GitHubID = p.Owner.GitHubId },
+                        AssignedUsers = p.UserProjects
+                            .Select(up => new UserDTO { ID = up.MemberId }) // Must fix the GitHub ID so it shows name instead of "null"
+                            .ToList(),
+                    })
+                    .ToListAsync();
+
+                return Ok(projects);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred when fetching projects.");
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+    }
+}
