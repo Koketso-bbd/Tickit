d4afca90f827c503926e992359587b9d8e852861
Koketso Motshana <Koketso.Motshana@bbd.co.za>
2025-02-24 10:51:37 +0200

    added endpoint for NotificationTypeController

---
+++ api/api/Controllers/NotificationTypeController.cs
@@ -0,0 +1,38 @@
+using api.Data;
+using api.DTOs;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationTypeController : ControllerBase
+    {
+        private readonly TickItDbContext _context;
+        private readonly ILogger<NotificationTypeController> _logger;
+
+        public NotificationTypeController(TickItDbContext context, ILogger<NotificationTypeController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<NotificationTypeDTO>>> GetNotificationTypes()
+        {
+           return await _context.NotificationTypes
+            .Select(nt => new NotificationTypeDTO
+            {
+                ID = nt.Id,
+                NotificationName = nt.NotificationName,
+            })
+            .ToListAsync();
+        }
+
+        
+    }
+}
