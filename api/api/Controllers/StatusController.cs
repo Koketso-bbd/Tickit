8173c9cf71094184dbdceef583fd4d046b36da8f
Koketso Motshana <Koketso.Motshana@bbd.co.za>
2025-02-24 15:48:22 +0200

    removed one endpoint(getbyid) in the StatusController.cs

--- api/api/Controllers/StatusController.cs
+++ api/api/Controllers/StatusController.cs
@@ -32,36 +32,6 @@ namespace api.Controllers
                .ToListAsync();
 
         }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<StatusDTO>> GetStatusById(int id)
-        {
-            try
-            {
-                var status = await _context.Statuses
-                    .Where(s => s.Id == id)
-                    .Select(s => new StatusDTO{
-                        Id = s.Id,
-                        StatusName = s.StatusName,
-                    })
-                    .FirstOrDefaultAsync();
-                
-                if(status == null)
-                {
-                    _logger.LogWarning("Status with ID{id} not found",id);
-                    return NotFound($"Status with ID {id} not found");
-                }
-
-                return Ok(status);
-            
-            }
-            catch(Exception ex)
-            {
-                _logger.LogError(ex, "An error occured when fetching the project.");
-                return StatusCode(500, "Internal Server Error");
-            }
-        }
-
         
     }
 
