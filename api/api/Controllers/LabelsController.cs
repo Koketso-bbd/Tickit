8ae9a13c4126ebada6eec5fdddf824ceae48b852
Andy Nkumane <Andy.Nkumane@bbd.co.za>
2025-02-24 13:47:38 +0200

    label controller: adding get method - get labels

--- api/api/Controllers/LabelsController.cs
+++ api/api/Controllers/LabelsController.cs
@@ -1,4 +1,5 @@
 using api.Data;
+using api.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +16,30 @@ namespace api.Controllers
         {
             _context = context;
             _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LabelDTO>>> GetLabels()
+        {
+            try
+            {
+                var labels = await _context.Labels
+                    .Select(l => new LabelDTO { ID = l.Id, LabelName = l.LabelName })
+                    .ToListAsync();
+                
+                
+                if (labels == null || !labels.Any())
+                {
+                    _logger.LogWarning("No labels have been found.");
+                    return NotFound("No labels found.");
+                }
+                return Ok(labels);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occured while fetching labels");
+                return StatusCode(500, "Internal Error");
+            }
         }
     }
 }
