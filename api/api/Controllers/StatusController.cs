e112934536f6f004863bddc59841deb18016833c
Koketso Motshana <Koketso.Motshana@bbd.co.za>
2025-02-24 09:46:20 +0200

    added status end points

---
+++ api/api/Controllers/StatusController.cs
@@ -0,0 +1,41 @@
+using api.Data;
+using api.DTOs;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private readonly TickItDbContext _context;
+        private readonly ILogger<StatusController> _logger;
+
+        public StatusController(TickItDbContext context, ILogger<StatusController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StatusDTO>>> GetStatuses()
+        {
+            return await _context.Statuses
+               .Select(s => new StatusDTO
+               {
+                   Id = s.Id,
+                   StatusName = s.StatusName,
+               })
+               .ToListAsync();
+
+        }
+
+        
+    }
+
+
+}
