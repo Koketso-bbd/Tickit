4e2b8928b643e20198caf4358574f15b7998d35a
Koketso Motshana <Koketso.Motshana@bbd.co.za>
2025-02-24 10:43:34 +0200

    StatusDTO added

---
+++ api/api/DTOs/StatusDTO.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace api.DTOs;
+
+public partial class StatusDTO
+{
+    public int Id { get; set; }
+
+    public string StatusName { get; set; } = null!;
+}
