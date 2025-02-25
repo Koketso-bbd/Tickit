a3a506d2c86cb74f5e5854b387b054d8d1df98e1
Andy Nkumane <Andy.Nkumane@bbd.co.za>
2025-02-25 11:11:37 +0200

    adding internal error for posting data

--- api/api/Helpers/HttpResponseHelper.cs
+++ api/api/Helpers/HttpResponseHelper.cs
@@ -2,18 +2,22 @@
 {
     public static class HttpResponseHelper
     {
-        public static (int StatusCode, string Message) InternalServerErrorGet(string errorFor, ILogger logger, Exception ex)
+        public static (int StatusCode, string Message) InternalServerErrorGet(string label, ILogger logger, Exception ex)
         {
-            logger.LogError(ex, $"An error occured while fetching {errorFor}.");
+            logger.LogError(ex, $"An error occured while fetching {label}.");
             return (500, "Internal Error");
         }
 
-        public static (int StatusCode, string Message) InternalServerErrorDelete(string errorFor, ILogger logger, Exception ex)
+        public static (int StatusCode, string Message) InternalServerErrorDelete(string label, ILogger logger, Exception ex)
         {
-            logger.LogError(ex, $"An error occured while deleting {errorFor}.");
+            logger.LogError(ex, $"An error occured while deleting {label}.");
             return (500, "Internal Error");
         }
 
-
+        public static (int StatusCode, string Message) InternalServerErrorPost(string label, ILogger logger, Exception ex)
+        {
+            logger.LogError(ex, $"An error occured while adding {label}.");
+            return (500, "Internal Error");
+        }
     }
 }
