1c5449ca7efe2767a13d16084431c44fcdc68ac8
Andy Nkumane <Andy.Nkumane@bbd.co.za>
2025-02-27 11:58:31 +0200

    tests: user controller - adding get user notifications returns list of notifications

--- api/api/Tests/UsersControllerTests.cs
+++ api/api/Tests/UsersControllerTests.cs
@@ -108,5 +108,62 @@ namespace api.Tests
 
             Assert.Equal("User not found", message);
         }
+
+        [Fact]
+        public async System.Threading.Tasks.Task GetUserNotifications_ReturnsListOfNotifications()
+        {
+            var controller = new UsersController(_dbContext, _loggerMock.Object);
+
+            var users = new List<User>
+            {
+                new() { Id = 1, GitHubId = "GitHub User 1" },
+                new() { Id = 2, GitHubId = "GitHub User 2" }
+            };
+            
+            await _dbContext.Users.AddRangeAsync(users);
+            await _dbContext.SaveChangesAsync();
+
+            var notifications = new List<Notification>
+            {
+                //user1 = 4 notifications; user2 = 3 notifications
+                new() { Id = 1, UserId = 1, ProjectId = 1, TaskId = 1, IsRead = false, Message = "notification 1", NotificationTypeId = 2, CreatedAt = new DateTime(2025,02,20)},
+                new() { Id = 2, UserId = 2, ProjectId = 1, TaskId = 2, IsRead = true, Message = "notification 2", NotificationTypeId = 1, CreatedAt = new DateTime(2025,02,20)},
+                new() { Id = 3, UserId = 1, ProjectId = 1, TaskId = 1, IsRead = true, Message = "notification 3", NotificationTypeId = 3, CreatedAt = new DateTime(2025,02,24)},
+                new() { Id = 4, UserId = 1, ProjectId = 2, TaskId = 12, IsRead = true, Message = "notification 4", NotificationTypeId = 2, CreatedAt = new DateTime(2025,02,25)},
+                new() { Id = 5, UserId = 2, ProjectId = 1, TaskId = 2, IsRead = false, Message = "notification 5", NotificationTypeId = 2, CreatedAt = new DateTime(2025,02,26)},
+                new() { Id = 6, UserId = 2, ProjectId = 3, TaskId = 1, IsRead = true, Message = "notification 6", NotificationTypeId = 1, CreatedAt = new DateTime(2025,02,27)},
+                new() { Id = 7, UserId = 1, ProjectId = 1, TaskId = 1, IsRead = false, Message = "notification 7", NotificationTypeId = 2, CreatedAt = new DateTime(2025,02,27)}
+            };
+
+            await _dbContext.Notifications.AddRangeAsync(notifications);
+            await _dbContext.SaveChangesAsync();
+
+            //User 1 notifications
+            var result1 = await controller.GetUserNotifications(1);
+            var okResult1 = Assert.IsType<OkObjectResult>(result1);
+            var returnedNotifications1 = Assert.IsAssignableFrom<List<NotificationsDTO>>(okResult1.Value);
+            
+            Assert.Equal(4, returnedNotifications1.Count);
+
+            var notification1 = returnedNotifications1[0];
+            Assert.Equal(1, notification1.UserId);
+            Assert.Equal("notification 1", notification1.Message);
+            Assert.False(notification1.IsRead);
+            Assert.Equal(1, notification1.TaskId);
+            Assert.Equal(1, notification1.Id);
+
+            var notification4 = returnedNotifications1[2];
+            Assert.Equal("notification 4", notification4.Message);
+            Assert.True(notification4.IsRead);
+            Assert.Equal(12, notification4.TaskId);
+            Assert.Equal(4, notification4.Id);
+
+            //User 2 notifications
+            var result2 = await controller.GetUserNotifications(2);
+            var okResult2 = Assert.IsType<OkObjectResult>(result2);
+            var returnedNotifications2 = Assert.IsAssignableFrom<List<NotificationsDTO>>(okResult2.Value);
+
+            Assert.Equal(3, returnedNotifications2.Count);
+        }
     }
 }
