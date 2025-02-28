db35b5edb859959ac461c769af048f463d4086d9
Koketso Motshana <Koketso.Motshana@bbd.co.za>
2025-02-28 10:26:15 +0200

    modified userproject tests file

--- api/api/Tests/UserProjectsControllerTests.cs
+++ api/api/Tests/UserProjectsControllerTests.cs
@@ -9,15 +9,15 @@ using Xunit;
 
 namespace api.Tests;
 
-
 public class UserProjectsTest
 {
     public class UserProjectsControllerTests
+
     {
         private readonly Mock<ILogger<UserProjectsController>> _loggerMock;
         private readonly UserProjectsController _controller;
-        private readonly DbContextOptions<TickItDbContext>? _dbContextOptions;
-        private readonly TickItDbContext? _dbContext;
+        private readonly DbContextOptions<TickItDbContext> _dbContextOptions;
+        private readonly TickItDbContext _dbContext;
 
         public UserProjectsControllerTests()
         {
@@ -33,6 +33,7 @@ public class UserProjectsTest
 
         [Fact]
         public void Dispose()
+
         {
             _dbContext?.Dispose();
         }
@@ -40,6 +41,7 @@ public class UserProjectsTest
 
         [Fact]
         public async System.Threading.Tasks.Task PostUserProjects_ShouldReturnNotFound_WhenUserDoesNotExist()
+        
         {
             await _dbContext.Projects.AddAsync(new Project { Id = 1, ProjectName = "Testing for when user does not exist" });
             await  _dbContext.Roles.AddAsync(new Role { Id = 1, RoleName = "Role" });
@@ -56,6 +58,7 @@ public class UserProjectsTest
         
         [Fact]
         public async System.Threading.Tasks.Task PostUserProjects_ShouldReturnNotFound_WhenProjectDoesNotExist()
+
         {
             await _dbContext.Users.AddAsync(new User { Id = 1, GitHubId = "Koki-98" });
             await _dbContext.Roles.AddAsync(new Role { Id = 1, RoleName = "Guest" });
@@ -71,6 +74,7 @@ public class UserProjectsTest
 
          [Fact]
         public async System.Threading.Tasks.Task PostUserProjects_ShouldReturnNotFound_WhenRoleDoesNotExist()
+        
         {
             await _dbContext.Users.AddAsync(new User { Id = 1, GitHubId = "Koki-98" });
             await _dbContext.Projects.AddAsync(new Project { Id = 1, ProjectName = "WhenRoleDoesNotExist" });
