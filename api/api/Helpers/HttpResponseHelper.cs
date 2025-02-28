e43e8d1585889c51b5fe9b84516ed44fcb8b154a
Andy Nkumane <Andy.Nkumane@bbd.co.za>
2025-02-28 09:46:33 +0200

    updating http reponse helper removing redundancy

--- api/api/Helpers/HttpResponseHelper.cs
+++ api/api/Helpers/HttpResponseHelper.cs
@@ -2,27 +2,9 @@
 {
     public static class HttpResponseHelper
     {
-        public static (int StatusCode, string Message) InternalServerErrorGet(string label, ILogger logger, Exception ex)
+        public static (int StatusCode, string Message) InternalServerError(string label, ILogger logger, Exception ex)
         {
-            logger.LogError(ex, $"An error occured while fetching {label}.");
-            return (500, "Internal Error");
-        }
-
-        public static (int StatusCode, string Message) InternalServerErrorDelete(string label, ILogger logger, Exception ex)
-        {
-            logger.LogError(ex, $"An error occured while deleting {label}.");
-            return (500, "Internal Error");
-        }
-
-        public static (int StatusCode, string Message) InternalServerErrorPost(string label, ILogger logger, Exception ex)
-        {
-            logger.LogError(ex, $"An error occured while adding {label}.");
-            return (500, "Internal Error");
-        }
-
-        public static (int StatusCode, string Message) InternalServerErrorPut(string label, ILogger logger, Exception ex)
-        {
-            logger.LogError(ex, $"An error occured while updating {label}.");
+            logger.LogError(ex, $"An error occured while {label}.");
             return (500, "Internal Error");
         }
     }
