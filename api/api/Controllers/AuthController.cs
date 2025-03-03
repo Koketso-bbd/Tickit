692028b4ad5ce7ac7c1b794008b83f763c7b3cb8
Scelo Khoza <scelo.khoza@bbd.co.za>
2025-03-03 15:02:27 +0200

    adding a google callback

--- api/api/Controllers/AuthController.cs
+++ api/api/Controllers/AuthController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Mvc;
@@ -11,5 +12,21 @@ public class AuthController : ControllerBase
     {
         return Challenge(new AuthenticationProperties { RedirectUri = "/api/auth/callback"},
         GoogleDefaults.AuthenticationScheme);
+    }
+
+    [HttpGet("callback")]
+    public async Task<IActionResult> GoogleCallback()
+    {
+        if (!User.Identity.IsAuthenticated) return Unauthorized();
+
+        var googleId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var email = User.FindFirst(ClaimTypes.Email)?.Value;
+        var jwtToken = await HttpContext.GetTokenAsync("id_token");
+
+        return Ok(new {
+            GoogleId = googleId,
+            Email = email,
+            IdToken = jwtToken 
+        });
     }
 }
