333ac76defa9a59df031661d9eb6f5ec387adfe8
Scelo Khoza <scelo.khoza@bbd.co.za>
2025-03-03 20:37:04 +0200

    figured the validation for swagger and it now works provided the endpoint is protected

--- api/api/Program.cs
+++ api/api/Program.cs
@@ -77,7 +77,7 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "TickIt API", Version = "v1" });
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Description = "Enter 'Bearer' followed by your token in the text input below.\nExample: Bearer {your_token}",
         Name = "Authorization",
         In = ParameterLocation.Header,
         Type = SecuritySchemeType.Http,
