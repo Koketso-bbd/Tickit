0213e3a718c55f93b78e3453afb96475b39a9e4b
Scelo Khoza <scelo.khoza@bbd.co.za>
2025-03-03 20:32:58 +0200

    specifying parameters that might possibly help in swagger being able to correctly validate the jwt token

--- api/api/Program.cs
+++ api/api/Program.cs
@@ -80,8 +80,9 @@ builder.Services.AddSwaggerGen(c =>
         Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
         Name = "Authorization",
         In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
     });
 
     c.AddSecurityRequirement(new OpenApiSecurityRequirement
