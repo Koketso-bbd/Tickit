a0b78168c6f477101653405dd71e0085ed9381ae
Scelo Khoza <scelo.khoza@bbd.co.za>
2025-03-05 14:39:19 +0200

    removing the tasklabel dto since it is antequated

--- api/DTOs/TaskDTO.cs
+++ api/DTOs/TaskDTO.cs
@@ -7,10 +7,10 @@ namespace api.DTOs;
 public class TaskDTO
 {
     [Required] public int AssigneeId { get; set; }
-    [Required, MaxLength(255)] public string TaskName { get; set; }
+    [Required] public string TaskName { get; set; }
     [Required] public int PriorityId { get; set; }
     [Required] public int ProjectId { get; set; }
-    [MaxLength(1000)] public string? TaskDescription { get; set; } = "No description provided";
+    public string? TaskDescription { get; set; } = "No description provided";
     public DateTime? DueDate { get; set; }
     public List<int>? ProjectLabelIds { get; set; }
 }
