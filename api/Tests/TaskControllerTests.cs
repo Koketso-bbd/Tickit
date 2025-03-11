dee69948868ff731863a1e6d9534ad9dd33118ce
Koketso Motshana <Koketso.Motshana@bbd.co.za>
2025-03-11 07:52:55 +0200

    fixed CreateTask_ReturnsBadRequest_WhenTaskNameLengthExceeds255 test

--- api/Tests/TaskControllerTests.cs
+++ api/Tests/TaskControllerTests.cs
@@ -272,9 +272,8 @@ namespace api.Tests
         }
 
         [Fact]
-        public async System.Threading.Tasks.Task CreateTask_ReturnsBadRequest_WhenTaskNameLengthExceeds255()
+        public async System.Threading.Tasks.Task CreateTask_ReturnsBadRequest_WhenTaskDescriptionLengthExceeds1000()
         {
-
             var assigneeID = 1;
             var projectId = 1;
             var user = new User
@@ -320,22 +319,50 @@ namespace api.Tests
         }
 
         [Fact]
-        public async System.Threading.Tasks.Task CreateTask_ReturnsBadRequest_WhenTaskDescriptionLengthExceeds1000()
+        public async System.Threading.Tasks.Task CreateTask_ReturnsBadRequest_WhenTaskNameLengthExceeds255()
+
         {
+            var assigneeID = 1;
+            var projectId = 1;
+            var user = new User
+            {
+                Id = 1,
+                GitHubId = "GitHub User 1"
+            };
+
+            var project = new Project
+            {
+                Id = projectId,
+                OwnerId = assigneeID,
+                ProjectName = "project 1",
+                ProjectDescription = "project description for project 1"
+            };
+
+            var userProject = new api.Models.UserProject
+            {
+                MemberId = assigneeID,
+                ProjectId = projectId,
+                RoleId = 2,
+            };
+
             var taskDto = new TaskDTO
             {
                 TaskName = new string('t', 256),
                 PriorityId = 1,
-                AssigneeId = 1,
+                AssigneeId = projectId,
                 TaskDescription = "Testing task",
                 DueDate = DateTime.UtcNow,
-                ProjectId = 1,
+                ProjectId = projectId,
             };
 
+            await _dbContext.Users.AddAsync(user);
+            await _dbContext.Projects.AddAsync(project);
+            await _dbContext.UserProjects.AddAsync(userProject);
+            await _dbContext.SaveChangesAsync();
             var result = await _controller.CreateTask(taskDto);
             var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
             var value = badRequestResult.Value as dynamic;
-            Assert.Equal("Task name cannot exceed 255 charcacters.", value.message.ToString());
+            Assert.Equal("Task name cannot exceed 255 characters.", value.message.ToString());
         }
 
         [Fact]
