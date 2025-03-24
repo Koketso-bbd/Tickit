33be37a0fce680baf5e8400070ebbe675bea4e30
Bradley <Bradley.Ranaka@bbd.co.za>
2025-03-24 11:38:12 +0200

    Added route for dashboard and children routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 export const routes: Routes = [
     {path: '', component: LandingComponent},
+    {path: 'dashboard', component: DashboardComponent, children: [
+        {path: '', component: DashboardComponent},
+        {path: 'projects', component: ProjectsComponent},
+        {path: 'tasks', component: TasksComponent}
+    ]},
     {path: '**', component: NotFoundComponent}
 ];
