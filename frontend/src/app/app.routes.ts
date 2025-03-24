153540a24740e5deaea010c1fdac72191f000632
Bradley <Bradley.Ranaka@bbd.co.za>
2025-03-24 13:47:13 +0200

    Added Notifications route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,15 +2,16 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { TasksComponent } from './components/tasks/tasks.component';
+import { ProjectsComponent } from './components/dashboard/projects/projects.component';
+import { TasksComponent } from './components/dashboard/tasks/tasks.component';
+import { NotificationsComponent } from './components/dashboard/notifications/notifications.component';
 
 export const routes: Routes = [
     {path: '', component: LandingComponent},
     {path: 'dashboard', component: DashboardComponent, children: [
-        {path: '', component: DashboardComponent},
         {path: 'projects', component: ProjectsComponent},
-        {path: 'tasks', component: TasksComponent}
+        {path: 'tasks', component: TasksComponent},
+        {path: 'notifications', component: NotificationsComponent}
     ]},
     {path: '**', component: NotFoundComponent}
 ];
