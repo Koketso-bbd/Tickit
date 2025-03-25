76168840094219c5226c3785c19e389d7524a3cd
Scelo Khoza <scelo.khoza@bbd.co.za>
2025-03-25 11:03:15 +0200

    implementing the componenent logic for the tasks

--- frontend/src/app/components/dashboard/tasks/tasks.component.ts
+++ frontend/src/app/components/dashboard/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../../services/task.service';
 
 @Component({
   selector: 'app-tasks',
@@ -6,6 +7,33 @@ import { Component } from '@angular/core';
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
+  response: any;
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  getData() {
+    this.taskService.fetchData('tasks').subscribe(
+      data => this.response = data
+    );
+  }
+
+  sendData(){
+    const payload = {
+
+    };
+
+    this.taskService.postData('tasks', payload).subscribe(
+      data => this.response = data
+    );
+  }
+
+  getStringifiedResponse(): string {
+    return this.response ? JSON.stringify(this.response, null, 2) : 'No data yet';
+  }
 
 }
