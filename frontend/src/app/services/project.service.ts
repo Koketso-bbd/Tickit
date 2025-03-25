d4828b1fecef4b9e5014a834e803b6160fbe3566
Bradley <Bradley.Ranaka@bbd.co.za>
2025-03-25 15:11:39 +0200

    Added Project Service

---
+++ frontend/src/app/services/project.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+
+  private token: string | null = localStorage.getItem('jwt');
+  private apiUrl: string = "https://localhost:7151/api/projects";
+  
+  constructor(private http: HttpClient) { }
+
+  private setHeaders() {
+    return new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`,
+      'Content-Type': 'application/json'
+    });
+  }
+
+  getProjects(): Observable<any> {
+    const headers = this.setHeaders();
+    return this.http.get<any[]>(`${this.apiUrl}`, { headers });
+  }
+}
