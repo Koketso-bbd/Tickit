af4ab1fb2a4f7e981e0b93590aa6f06eccc48962
Bradley <Bradley.Ranaka@bbd.co.za>
2025-03-25 15:26:14 +0200

    fetch projects logic added

--- frontend/src/app/components/dashboard/projects/projects.component.ts
+++ frontend/src/app/components/dashboard/projects/projects.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { ProjectService } from '../../../services/project.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
-export class ProjectsComponent {
 
+export class ProjectsComponent {
+  projects: any[] = [];
+  constructor(private projectService: ProjectService) { }
+
+  ngOnInit(): void {
+    this.fetchProjects();
+  }
+
+  fetchProjects() {
+    this.projectService.getProjects().subscribe({
+      next: (data) => {
+        this.projects = data;
+        console.log(this.projects);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    })
+  }
 }
