29d1aeabaca9f530c256b2e090a32cf7333c6f27
Koketso Motshana <Koketso.Motshana@bbd.co.za>
2025-03-25 16:34:39 +0200

    added the profile component

---
+++ frontend/src/app/components/dashboard/profile/profile.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+
+  userName: string = localStorage.getItem('fullname') ?? '';
+  email: string = localStorage.getItem('email') ?? '';
+  userImage: string = localStorage.getItem('picture') ?? '';
+
+
+  constructor(private router: Router) { }
+
+  logout() {
+    localStorage.removeItem('jwt');
+    localStorage.removeItem('email');
+    localStorage.removeItem('fullname');
+    localStorage.removeItem('picture');
+    this.router.navigate(['/login']);
+   }
+}
