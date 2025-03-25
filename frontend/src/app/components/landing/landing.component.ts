7e35ab87745809a6c554eebae9c910bf4429eb40
Andy Nkumane <Andy.Nkumane@bbd.co.za>
2025-03-25 11:50:36 +0200

    updating landing to redirect to the login page

--- frontend/src/app/components/landing/landing.component.ts
+++ frontend/src/app/components/landing/landing.component.ts
@@ -1,11 +1,16 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
 })
 export class LandingComponent {
+  constructor(private router: Router) { }
 
+  goToLogin(): void {
+    this.router.navigate(['/login']);
+  }
 }
