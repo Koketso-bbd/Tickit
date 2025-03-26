9147995048ae7daaea83a187cd5a7dc5db1bf6f3
Andy Nkumane <Andy.Nkumane@bbd.co.za>
2025-03-26 11:46:18 +0200

    adding temporary button to check how the task view would look like when working properly

--- frontend/src/app/components/dashboard/tasks/tasks.component.ts
+++ frontend/src/app/components/dashboard/tasks/tasks.component.ts
@@ -1,11 +1,22 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { TaskViewComponent } from '../../task-view/task-view.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [TaskViewComponent, NgIf],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
+  taskId: number = 120;
+  taskViewVisible = false;
 
+  openTaskView(): void {
+    this.taskViewVisible = true;  
+  }
+
+  closeTaskView(): void {
+    this.taskViewVisible = false;  
+  }
 }
