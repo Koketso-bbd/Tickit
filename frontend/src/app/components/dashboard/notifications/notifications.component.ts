c18425c669435c7f7023b093a090c114cdcce835
Koketso Motshana <Koketso.Motshana@bbd.co.za>
2025-03-26 20:58:56 +0200

    Forgot to commit this file after fixing conflicts

--- frontend/src/app/components/dashboard/notifications/notifications.component.ts
+++ frontend/src/app/components/dashboard/notifications/notifications.component.ts
@@ -1,36 +1,14 @@
-import { Component, OnInit, OnInit } from '@angular/core';
-import { DatePipe, NgFor, CommonModule } from '@angular/common';
-import { NotificationsService } from '../../../notifications.service';
+import { Component, OnInit} from '@angular/core';
 import { DatePipe, NgFor, CommonModule } from '@angular/common';
 import { NotificationsService } from '../../../services/notifications.service';
 
 @Component({
   selector: 'app-notifications',
   imports: [NgFor, DatePipe, CommonModule],
-  imports: [NgFor, DatePipe, CommonModule],
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.css'],
-  styleUrls: ['./notifications.component.css'],
 })
-export class NotificationsComponent implements OnInit {
-  notifications: any[] = [];
 
-  constructor(private notificationService: NotificationsService) {}
-
-  ngOnInit(): void {
-    this.getNotifications();
-  }
-
-  getNotifications(): void {
-    this.notificationService
-      .getNotifications()
-      .subscribe((notifications) => (this.notifications = notifications));
-  }
-
-  toggleReadStatus(notification: any): void {
-    notification.isRead = !notification.isRead;
-  }
-}
 
 export class NotificationsComponent implements OnInit {
   notifications: any[] = [];
