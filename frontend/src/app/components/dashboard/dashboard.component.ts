fff2899aeee6ab63ad466cf6ea7319d6646a01d6
Andy Nkumane <Andy.Nkumane@bbd.co.za>
2025-03-30 17:12:19 +0200

    side nav is a burger menu - default it open

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgClass],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
@@ -12,6 +13,16 @@ export class DashboardComponent {
   userImage: string = localStorage.getItem('picture') ?? '';
   
   constructor(private router: Router) { }
+
+  showMenu = true;
+
+  toggleMenu() {
+    const burgerButton = document.querySelector('.burger-button');
+    if (burgerButton) {
+      burgerButton.classList.toggle('active');
+    }
+    this.showMenu = !this.showMenu;
+  }
 
   logout() {
     localStorage.removeItem('jwt');
